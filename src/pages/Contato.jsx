ce802ad27fa5af1c58848006982cb04667eb3df6
Matheus <matheus.salvador2206@gmail.com>
2025-04-09 23:14:22 -0300

    Adição botão voltar home

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Contato() {
   return (
     <div className="min-h-screen bg-gray-900">
+      {/* Botão Voltar */}
+      <div className="container mx-auto px-4 py-4">
+        <Link
+          to="/"
+          className="inline-flex items-center text-green-500 hover:text-green-400 transition duration-300"
+        >
+          <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+          </svg>
+          Voltar para a página inicial
+        </Link>
+      </div>
+
       {/* Header */}
       <header className="py-16 bg-gray-800">
         <div className="container mx-auto px-4">
